ac7eb42f9095406c82e6f71c151b4090ac59d85c
re1fly <reflyfa@gmail.com>
2021-01-27 09:10:10 +0800

    html editor with preview

--- resources/js/components/pages/Announcement.js
+++ resources/js/components/pages/Announcement.js
@@ -1,37 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import {Button, Card} from "react-bootstrap";
+import ReactHtmlParser from "react-html-parser";
 
-class Announcement extends React.Component {
-    handleEditorChange = (e) => {
-        console.log(
-            'Content was updated:',
-            e.target.getContent()
-        );
-    }
+export default function Announcement() {
+    const [wysiwyg, setWysiwyg] = useState("");
+    const [parsed, setParsed] = useState("");
 
-    render() {
+    const handleEditorChange = content => {
+        setWysiwyg(content);
+        setParsed(content);
+    };
         return (
+            <Card style={{border: "none"}}>
+                <h3 className="mb-5 mt-4 text-center">Preview Announcement</h3>
+                <Card className="mb-4" style={{borderColor: "black", borderWidth:"3px", height:"400px" }}>
+                    <div className="wysiwyg">{wysiwyg && ReactHtmlParser(wysiwyg)}</div>
+                    </Card>
+                <Card className="text-center" style={{ marginBottom: "10%", width: "50%", marginLeft:"25%", border: "none" }}>
             <Editor
                 apiKey="ot65hmw48i01kedcx33fd4nmbqssc98qb9tzj7gnmwszjo2a"
                 initialValue="<p>Initial content</p>"
                 init={{
-                    height: 300,
-                    menubar: false,
+                    height: 200,
+                    selector: 'textarea',
+                    // menubar: 'view',
                     plugins: [
                         'advlist autolink lists link image',
                         'charmap print preview anchor help',
                         'searchreplace visualblocks code',
-                        'insertdatetime media table paste wordcount'
+                        'insertdatetime media table paste wordcount',
+                        'preview'
                     ],
                     toolbar:
-                        'undo redo | formatselect | bold italic | \
-                        alignleft aligncenter alignright | \
-                        bullist numlist outdent indent | help'
+                    'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl'
                 }}
-                onChange={this.handleEditorChange}
+                onEditorChange={handleEditorChange}
             />
+                    <Button  className="mt-4" variant="secondary" type="submit" >
+                        Save Announcement
+                    </Button>
+                </Card>
+            </Card>
         );
-    }
 }
-
-export default Announcement;
--- resources/js/components/pages/Dashboard.js
+++ resources/js/components/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Header from '../layout/Header';
 import Sidebar from '../layout/Sidebar';
 import Announcement from "./Announcement";
-import {Button, Card} from "react-bootstrap";
+
 
 class Dashboard extends Component {
     render() {
@@ -14,16 +14,7 @@ class Dashboard extends Component {
                         <Sidebar />
                     </div>
                     <div className="col-10">
-                        <Card className="text-center" style={{border: "none"}}>
-                           <h3 className="mb-5 mt-4">Preview Announcement</h3>
-                            <Card className="text-center" style={{ marginBottom: "10%", width: "50%", marginLeft:"25%", border: "none" }}>
-                                <p className="mb-4">preview</p>
                         <Announcement />
-                            <Button  className="mt-4" variant="secondary" type="submit" >
-                                Save Announcement
-                            </Button>
-                            </Card>
-                        </Card>
                     </div>
                 </div>
 
