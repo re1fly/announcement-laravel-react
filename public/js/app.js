2f4fe5fbd533ae879b25556d061ebddce183e191
re1fly <reflyfa@gmail.com>
2021-01-30 17:05:21 +0800

    text editor with image but must refresh

--- public/js/app.js
+++ public/js/app.js
@@ -23132,10 +23132,48 @@ function Announcement() {
           init: {
             height: 200,
             selector: 'textarea#full-featured-non-premium',
-            plugins: ['print preview paste importcss searchreplace autolink autosave directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap emoticons'],
+            plugins: ['print preview paste importcss searchreplace autolink autosave directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap emoticons image code'],
             menubar: 'file edit view insert format tools table tc help',
-            toolbar: ' a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table | undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
-            toolbar_mode: 'floating'
+            toolbar: ' a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table | link image | undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+            toolbar_mode: 'floating',
+            image_title: true,
+            automatic_uploads: true,
+            file_picker_types: 'image',
+
+            /* and here's our custom image picker*/
+            file_picker_callback: function file_picker_callback(cb, value, meta) {
+              var input = document.createElement('input');
+              input.setAttribute('type', 'file');
+              input.setAttribute('accept', 'image/*');
+
+              input.onchange = function () {
+                var file = this.files[0];
+                var reader = new FileReader();
+
+                reader.onload = function () {
+                  /*
+                    Note: Now we need to register the blob in TinyMCEs image blob
+                    registry. In the next release this part hopefully won't be
+                    necessary, as we are looking to handle it internally.
+                  */
+                  var id = 'blobid' + new Date().getTime();
+                  var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                  var base64 = reader.result.split(',')[1];
+                  var blobInfo = blobCache.create(id, file, base64);
+                  blobCache.add(blobInfo);
+                  /* call the callback and populate the Title field with the file name */
+
+                  cb(blobInfo.blobUri(), {
+                    title: file.name
+                  });
+                };
+
+                reader.readAsDataURL(file);
+              };
+
+              input.click();
+            },
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
           },
           onEditorChange: handleEditorChange
         }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core_Box__WEBPACK_IMPORTED_MODULE_10__.default, {
